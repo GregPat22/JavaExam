0abe486b2ba196b3777a9da634d3bf863a5329ce
Greg <ninopatini@icloud.com>
2024-04-30 17:04:51 +0200

    Second Exercise and started the third one

---
+++ Esercizio2.java
@@ -0,0 +1,29 @@
+public class Esercizio2 {
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 6;
+
+        System.out.println("Il primo numero è: " + a);
+
+        System.out.println("Il secondo numero è: " + b);
+
+        // ----------operaizioni
+
+        int sum = a + b;
+        int diff = a - b;
+        int molt = a * b;
+        int div = a / b;
+
+        // ------------
+
+        System.out.println("Il risultato di 7 + 6 è: " + sum);
+
+        System.out.println("Il risultato di 7 - 6 è: " + diff);
+
+        System.out.println("Il risultato di 7 * 6 è: " + molt);
+
+        System.out.println("Il risultato di 7 / 6 è: " + div);
+
+    }
+}
---
+++ Esercizio3.java
@@ -0,0 +1,7 @@
+public class Esercizio3 {
+    public static void main(String[] args) {
+
+        System.out.println("Calcolo il numero di biglietti disponibili e l'incasso totale dei biglietti venduti ");
+
+    }
+}
